ae0083ae34f116fca0df8d267c2808302d3fb3ee
Abhilashasingla <abhilashasingla.as@gmail.com>
2021-12-30 03:06:50 +0530

    first commit

---
+++ src/test/java/RestAPI/Example/Setup.java
@@ -0,0 +1,10 @@
+package RestAPI.Example;
+import org.testng.annotations.Test;
+
+public class Setup {
+	
+	@Test
+	  public void SetupMethod() {
+		  System.out.println("Setup method invoked");
+	  }
+}
---
+++ src/test/java/RestAPI/Example/TearDown.java
@@ -0,0 +1,11 @@
+package RestAPI.Example;
+
+import org.testng.annotations.Test;
+
+public class TearDown {
+	@Test
+	  public void TearDownMethod() {
+		  System.out.println("Tear Down method invoked");
+	  }
+
+}
